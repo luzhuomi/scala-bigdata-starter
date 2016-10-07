971b641f5c8ebe476cd287b966c07545ecfa896f
Kenny Lu <luzhuomi@gmail.com>
2016-10-07 14:33:17 +0800

    updated

--- spark-ml-examples/src/main/scala/VectorExampleShell.scala
+++ spark-ml-examples/src/main/scala/VectorExampleShell.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-// import org.apache.spark.rdd._
+import org.apache.spark.rdd._
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.stat.Statistics
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
